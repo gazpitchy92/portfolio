0d85727c0397b7ddd7a64966ac1bd4e787a3bf6a
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2025-07-24 16:46:11 +0100

    host port 80

--- host.sh
+++ host.sh
@@ -2,4 +2,4 @@
 
 git reset --hard HEAD
 git pull
-docker run --rm -it   -v "$(pwd):/app"   -w /app   -p 5173:5173   node:lts   sh -c "npm install && npm run dev -- --host"
+docker run --rm -it   -v "$(pwd):/app"   -w /app   -p 80:5173   node:lts   sh -c "npm install && npm run dev -- --host"
