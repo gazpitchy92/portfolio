99e6a27ccb13e847dd16aa851e13dca0ac4ee377
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2025-08-21 20:05:58 +0100

    hmr

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    hmr: {
+      overlay: false,
+    },
     host: '0.0.0.0',  
     allowedHosts: [
       'gpitchford.com',
